ce2141db2db671a5102b9434e7792dc343ca0cff
timotheetacca <66522009+timotheetacca@users.noreply.github.com>
2023-11-16 15:05:26 +0100

    add 'list_presidents_names()'

--- main.py
+++ main.py
@@ -27,6 +27,16 @@ def associate_name(name):
         # If the name doesn't exist in the dictionary, return an error message
         return f"The name {name} doesn't exist in our data base ⚠ "
 
+
+
+
+def list_presidents_names(directory, extension):
+    #Make a set of the extract_president_names() to avoid duplicates
+    president_names = set(extract_president_names(directory, extension))
+    return president_names
+
+
+
 # Call of the function extract_name()
 presidents = extract_president_names("speeches", "txt")
 for president in presidents:
@@ -36,4 +46,9 @@ print("")
 # Call of the function associate_name()
 print(associate_name("Chirac"))
 print(associate_name("Julien"))
+print("")
+
+# Call of the function list_presidents_names()
+for name in (list_presidents_names("speeches", "txt")):
+    print(name)
 print("")
