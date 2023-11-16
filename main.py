68f1f88eeaa08e846a5bab5990cd586ff88f7b99
Julien-Oudin <julien.oudin@efrei.net>
2023-11-16 14:59:31 +0100

    add 'associate_name'() function

--- main.py
+++ main.py
@@ -17,8 +17,23 @@ def extract_president_names(directory, extension):
     return files_presidents
 
 
+
+def associate_name(name):
+    first_names = {"Chirac": "Jacques", "Giscard d'Estaing": "Valéry", "Mitterrand": "François", "Macron": "Emmanuel", "Sarkozy": "Nicolas"}
+    if name in first_names:
+        # If the name exists in the dictionary, return the first name and a message
+        return first_names[name]+" "+name+"'s first name is : "+first_names[name]
+    else:
+        # If the name doesn't exist in the dictionary, return an error message
+        return f"The name {name} doesn't exist in our data base ⚠ "
+
 # Call of the function extract_name()
 presidents = extract_president_names("speeches", "txt")
 for president in presidents:
     print(president)
+print("")
+
+# Call of the function associate_name()
+print(associate_name("Chirac"))
+print(associate_name("Julien"))
 print("")
